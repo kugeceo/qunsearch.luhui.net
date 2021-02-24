59e44c3e5a8324062e1720db1e1e7216e0401453
鲁虺 <37980424+kugeceo@users.noreply.github.com>
2021-02-24 10:25:52 +0800

    Add files via upload

--- search/js/UniSearchData.js
+++ search/js/UniSearchData.js
@@ -248,53 +248,13 @@ UniSearch.data["zh-cn"]["video"] = {
 UniSearch.data["zh-cn"]["bt"] = {
     title: "磁力",
     items: {
-        gougou: {
-            title: "狗狗影视",
-            subtitle: "BT",
-            url: "https://www.gougou.com/search?id=28416&search="
+        bing: {
+            title: "必",
+            subtitle: "应",
+            url: "https://www.bing.com/search?q=magnet:?xt=urn:btih: "
         },
-		
-                "baiduxt": { title: "百度",
-			url: "http://www.baidu.com/baidu?tn=luhuinet&word=xt urn:btih  {keyword:gb2312}& http://baidu.luhui.net:70/{keyword:gb2312}&http://www.cilisou.cn/s.php?q={keyword:gb2312}" },
-
-		"bingxt": { title: "必应",
-			url: "http://www.bing.com/search?q=xt urn:btih {keyword}" },
-
-		"thunder": { title: "thunder",
-			url: "http://www.luhui.net:70/thunder://{keyword:gb2312}" },
-
-		"luhui": { title: "鲁虺", subtitle: "磁力搜索引擎",
-			url: "http://www.luhui.net:70/xt urn:btih {keyword:gb2312}" },
-
-		"qqdl": { title: "旋风", subtitle: "旋风链接",
-			url: "http://www.luhui.net:70/qqdl:// {keyword:gb2312}" },
-
-		"soxt": { title: "360", subtitle: "奇虎360旗下搜索服务",
-			url: "http://www.so.com/index.php?a=index&ie=utf-8&shb=1&src=www.luhui.net&q=xt urn:btih {keyword:gb2312}" },
-
-		"xmbt": { title: "小米", subtitle: "磁力搜索引擎",
-			url: "http://www.xmbt.org/cvyun-{keyword:gb2312}-0-0-1.html" },
-
-
-
-"sogouxt": { title: "搜狗",
-			url: "http://www.sogou.com/web?query=xt urn:btih  {keyword}" },
-
-
-		"zhizhuso": { title: "btroom", subtitle: "磁力搜索引擎",
-			url: "http://www.btroom5.com/word.html?kw={keyword:gb2312}" },
-
-
-		"btbili": { title: "btbili", subtitle: "磁力搜索引擎",
-			url: "http://www.btbili.org/main-search-kw-{keyword:gb2312}.html" },
-
-		"btdiggjx": { title: "BtDigg", subtitle: "磁力搜索引擎",
-			url: "http://www.btdiggzw.cc/s/{keyword:gb2312}" },
-		
-		
-		
         baidu: {
-            title: "baidu",
+            title: "百度",
             url: "https://www.baidu.com/baidu?tn=luhuinet&word=magnet:?xt=urn:btih: "
         Verycd: {
             title: "Verycd",
