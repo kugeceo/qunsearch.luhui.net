36077ae47d332c9d9c565114f9dedeeda6ee57c7
鲁虺 <37980424+kugeceo@users.noreply.github.com>
2021-02-28 07:55:32 +0800

    Add files via upload

--- search/js/UniSearchData.js
+++ search/js/UniSearchData.js
@@ -694,10 +694,6 @@ UniSearch.data["zh-cn"]["blog"] = {
             title: "教",
             subtitle: "育",
             url: "https://www.baidu.com/baidu?tn=luhuinet&word=site%3A.edu {keyword:gb2312}"
-		
-		kldni: {
-            title: "linkdomain:外链查询",
-            url: "https://www.baidu.com/baidu?tn=luhuinet&word=linkdomain: {keyword:gb2312}"
         }
     }
 };
