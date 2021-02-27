1b7c8aa1c3a852070476d427dbc82ad33dc2a103
鲁虺 <37980424+kugeceo@users.noreply.github.com>
2021-02-27 12:19:31 +0800

    Add files via upload

--- search/v2.0.1/js/keywords.js
+++ search/v2.0.1/js/keywords.js
@@ -1,13 +1,24 @@
-<html>
-<head><title>404 Not Found</title></head>
-<body bgcolor="white">
-<center><h1>404 Not Found</h1></center>
-<hr><center>nginx/1.2.0</center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+(function() {
+    clear();
+    const words = [];
+
+    function uniq(value, index, self) { 
+        return self.indexOf(value) === index;
+    }
+   
+    let selector = `#rso > div > div > div > div > div em`;       
+    const found = document.querySelectorAll(selector);       
+    if (found.length > 0) {
+        found.forEach((f) => {
+            let text = f.textContent;                                                           
+            if (text) {
+                words.push(text);
+            }
+        });
+    }
+
+    console.log("%cLOWER CASE", "font-size:14px;font-weight:bold");
+    console.log(words.map(v => v.toLowerCase()).filter(uniq).join("\n"));
+    console.log("%cALL", "font-size:14px;font-weight:bold;");
+    console.log(words.filter(uniq).join("\n"));    
+})();
